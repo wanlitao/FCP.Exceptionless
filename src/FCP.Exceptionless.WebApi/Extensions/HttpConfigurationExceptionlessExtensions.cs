02a00f18e25c8e06b185da865bf966e3262bfb45
万里涛 <wanlitao_3055601@outlook.com>
2016-12-02 21:22:28 +0800

    add WebApi Exceptionless Integration

---
+++ src/FCP.Exceptionless.WebApi/Extensions/HttpConfigurationExceptionlessExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Exceptionless;
+using FCP.Util;
+using System;
+using System.Web.Http;
+
+namespace FCP.Exceptionless.WebApi
+{
+    public static class HttpConfigurationExceptionlessExtensions
+    {
+        public static HttpConfiguration UseExceptionless(this HttpConfiguration configuration, string apiKey, string serverUrl = null)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            if (apiKey.isNullOrEmpty())
+                throw new ArgumentNullException(nameof(apiKey));            
+
+            var exceptionlessClient = new ExceptionlessClient(config =>
+            {
+                config.ApiKey = apiKey;
+
+                if (!serverUrl.isNullOrEmpty())
+                    config.ServerUrl = serverUrl;
+            });
+
+            exceptionlessClient.RegisterWebApi(configuration);
+
+            return configuration;
+        }
+    }
+}
